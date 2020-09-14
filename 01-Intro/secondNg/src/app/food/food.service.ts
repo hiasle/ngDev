ce1be79ef67b8898caabcb87d178f706f9144203
hiasle <matthias.hube@gmail.com>
2020-09-14 14:07:00 +0200

    food data from service

--- 01-Intro/secondNg/src/app/food/food.service.ts
+++ 01-Intro/secondNg/src/app/food/food.service.ts
@@ -1,19 +1,22 @@
 import { Injectable } from '@angular/core';
 import { of, Observable } from 'rxjs';
 import { FoodItem } from './food-item.model';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class FoodService {
-  constructor() {}
+  constructor(private httpClient: HttpClient) {}
 
-  food = [
+  /* food = [
     { name: 'Pommes', price: 2 },
     { name: 'Falaffel', price: 2 },
-  ];
+  ]; */
 
   getFood(): Observable<FoodItem[]> {
-    return of(this.food);
+    // return of(this.food);
+    return this.httpClient.get<FoodItem[]>(environment.url);
   }
 }
