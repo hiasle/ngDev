8f22cc96865d44a6eabd468ba4c565cb9c0adba0
hiasle <matthias.hube@gmail.com>
2020-09-17 12:49:02 +0200

    sum component finished

---
+++ 07-Reactive-Programming-and-State/Reactive/src/app/demos/samples/stateful/sum/sum.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { StatefulVoucherService } from '../stateful-voucher.service';
+import { getAllVouchers } from '../../../store/selectors/demos.selector';
+import { Voucher } from '../../model';
+
+@Component({
+  selector: 'app-sum',
+  templateUrl: './sum.component.html',
+  styleUrls: ['./sum.component.scss'],
+})
+export class SumComponent implements OnInit {
+  constructor(private vs: StatefulVoucherService) {}
+
+  sum = 0;
+
+  ngOnInit(): void {
+    this.vs.getAllVouchers().subscribe((vouchers: Voucher[]) => {
+      this.sum = 0;
+      vouchers.forEach((v) => (this.sum += v.Amount));
+    });
+  }
+}
